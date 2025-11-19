aaecf22b54b8834e50b2df594e019fd76f588a78
ezrandc <ezrandc@gmail.com>
2025-11-19 14:54:41 +0800

    Add SQL query for category revenue mix analysis

---
+++ Dashboard/Dashboard_sql/category_revenue_mix.sql
@@ -0,0 +1,15 @@
+SELECT
+    "Category",
+    EXTRACT(YEAR FROM ("Order Date")::date)::int AS year,
+    ROUND(
+        (SUM("Sales")::numeric) /
+        SUM(SUM("Sales")::numeric) OVER (
+            PARTITION BY EXTRACT(YEAR FROM ("Order Date")::date)) * 100,
+        2) AS revenue_mix
+FROM clean_data
+GROUP BY
+    "Category",
+    EXTRACT(YEAR FROM ("Order Date")::date)
+ORDER BY
+    "Category",
+    EXTRACT(YEAR FROM ("Order Date")::date);
